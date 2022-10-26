a13dcdf3d6b490c95d542e584adb354f7b104019
Ilay Sokolov <sokol8807@gmail.com>
2022-10-26 20:00:44 +0300

    initial comit

---
+++ StateAndDataFlow/UserManager.swift
@@ -0,0 +1,21 @@
+//
+//  UserManager.swift
+//  StateAndDataFlow
+//
+//  Created by brubru on 24.10.2022.
+//
+
+import Foundation
+
+class UserManager: ObservableObject {
+    @Published var isRegister = false
+    var name = ""
+}
+
+class DataStore {
+    static let shared = DataStore()
+    
+    var name = {}
+    
+    private init() {}
+}
